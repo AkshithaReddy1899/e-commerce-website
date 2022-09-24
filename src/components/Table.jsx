878d1b86e1301c2f5babac02c923a140efa590f6
Akshitha Reddy <akshithareddy1899@gmail.com>
2022-09-24 10:36:46 +0530

    Fix styling

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -26,69 +26,68 @@ const Table = (data) => {
 		dispatch(addToCart(id));
 	};
 
-	return (
-		<div className="flex flex-col">
-			<div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-				<div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-					<div className="overflow-x-auto">
-						<table className="min-w-full">
-							<thead className="border-b">
-								<tr>
-									<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-										Product Image
-									</th>
-									<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-										Product Name
-									</th>
-									<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-										Brand
-									</th>
-									<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-										Stock
-									</th>
-									<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-										Price
-									</th>
-									<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-										Buy
-									</th>
+	return (<div className="flex flex-col">
+  <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+    <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+      <div className="overflow-hidden">
+        <table className="min-w-full">
+          <thead className="border-b">
+					  <tr>
+							<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-center">
+								Product Image
+							</th>
+							<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-center">
+								Product Name
+							</th>
+							<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-center">
+								Brand
+							</th>
+							<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-center">
+								Stock
+							</th>
+							<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-center">
+								Price
+							</th>
+							<th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-center">
+								Buy
+							</th>
+						</tr>
+          </thead>
+          <tbody>
+					  {
+							products.map((item) => (
+								<tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100" key={item.id}>
+									<td className="px-auto text-center py-4 w-40 text-sm font-medium text-gray-900">
+										<img src={item.image} alt={item.title} className="w-14 mx-auto" />
+									</td>
+									<td className="text-sm text-gray-900 text-center font-light px-6 py-4 w-40">
+										{item.name}
+									</td>
+									<td className="text-sm text-gray-900 text-center font-light px-6 py-4 w-40">
+										{item.brand}
+									</td>
+									<td className="text-sm text-gray-900 text-center font-light px-6 py-4 w-40">
+										{(item.stock > 10) ? <p className="text-green-500 font-bold">In Stock</p> : <p className="text-red-400 font-bold">No Stock</p>}
+									</td>
+									<td className="text-sm text-gray-900 text-center font-light px-6 py-4 w-40">
+										${item.price}
+									</td>
+									<td className="text-sm text-gray-900 text-center font-light px-6 py-4 w-40">
+									<div className="flex justify-center items-center flex-row">
+										<input type="number" name={item.title} defaultValue={item.quantity} onChange={ (e) => handleChange(e, item) } className="bg-slate-200 w-9 mx-2 text-center" />
+										<p>Cart</p>
+										<input type="checkbox" className="m-1" id={item.id} name={item.title} onClick={ (e) => handleClick(e) } />
+									</div>
+									</td>
 								</tr>
-							</thead>
-							<tbody>
-								{
-									products.map((item) => (
-										<tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100" key={item.id}>
-											<td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-											  <img src={item.image} alt={item.title} className="w-14" />
-											</td>
-											<td className="text-sm text-gray-900 font-light px-6 py-4 w-12">
-												{item.name}
-											</td>
-											<td className="text-sm text-gray-900 font-light px-6 py-4 w-12">
-												{item.brand}
-											</td>
-											<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-											  {(item.stock > 10) ? <p className="text-green-500 font-bold">In Stock</p> : <p className="text-red-400 font-bold">No Stock</p>}
-											</td>
-											<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-												${item.price}
-											</td>
-											<td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-											<div className="flex justify-between items-center flex-row">
-												<input type="number" name={item.title} defaultValue={item.quantity} onChange={ (e) => handleChange(e, item) } className="bg-slate-200 w-9 mx-2 text-center" />
-												<p>Cart</p>
-												<input type="checkbox" className="m-1" id={item.id} name={item.title} onClick={ (e) => handleClick(e) } />
-											</div>
-											</td>
-										</tr>
-									))
-								}
-							</tbody>
-						</table>
-					</div>
-				</div>
-			</div>
-		</div>
+							))
+						}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+</div>
 	);
 };
 
